02579e24e0b05c90249b27ffb0bee32d091ef9d4
灘波竜星 <nambaryusei@nadaharyuuseinoMacBook-Pro.local>
2024-03-02 17:23:37 +0900

    変更内容の説明

--- upload.py
+++ upload.py
@@ -2,20 +2,58 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import boto3
 from werkzeug.utils import secure_filename
-import os
 from datetime import datetime
+import azure.cognitiveservices.speech as speechsdk
+from pydub import AudioSegment
+import os
+import uuid
+import tempfile
 
 app = Flask(__name__)
 CORS(app)
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+speech_key = os.environ['AZURE_SPEECH_KEY']
+service_region = os.environ['AZURE_SERVICE_REGION']
 S3_BUCKET_NAME = "testunity1.0"
 AWS_REGION = "ap-northeast-3"
+# 一時ディレクトリの作成
+output_directory = tempfile.mkdtemp()
 
 s3 = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1')
 table = dynamodb.Table('maindatabase')
+
+def text_to_speech(text, text_id):
+    # タイトルからファイル名を生成
+    sanitized_text = secure_filename(text)
+    base_filename = f"{sanitized_text}_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex}"
+    wav_filename = os.path.join(output_directory, f"{base_filename}.wav")
+    mp3_filename = os.path.join(output_directory, f"{base_filename}.mp3")
+    
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=wav_filename)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    
+    ssml_string = f"""
+    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
+           xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="ja-JP">
+        <voice name="ja-JP-DaichiNeural">
+            <mstts:express-as style="customerservice" styledegree="3">
+                {text}
+            </mstts:express-as>
+        </voice>
+    </speak>"""
+
+    synthesizer.speak_ssml_async(ssml_string).get()
+    
+    # WAVからMP3への変換
+    audio = AudioSegment.from_wav(wav_filename)
+    audio.export(mp3_filename, format="mp3")
+    os.remove(wav_filename)  # WAVファイルの削除
+
+    return mp3_filename
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -95,7 +133,47 @@ def delete_file():
         return jsonify({"message": "Delete successful"})
     except Exception as e:
         return jsonify({"message": "Error deleting item", "error": str(e)}), 500
+    
+@app.route('/upload_extended', methods=['POST'])
+def upload_extended():
+    text_id = request.form['text_id']
+    additional_text = request.form['additional_text']
+    description = request.form['description']
+    thumbnail = request.files['thumbnail']
+    media = request.files['media']
+    responses = []
 
+    # サムネイルとメディアファイルのアップロード処理
+    for file, file_type in [(thumbnail, 'thumbnail'), (media, 'media')]:
+        if file.filename:
+            filename = secure_filename(file.filename)
+            filepath = f'uploads/{text_id}/{file_type}/{filename}'
+            s3.upload_fileobj(file, S3_BUCKET_NAME, filepath)
+            file_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{filepath}"
 
-if __name__ == '__main__':
+            # DynamoDBに保存するためのレコード情報を更新
+            responses.append({"type": file_type, "file_url": file_url})
+
+    # 説明文から音声を生成し、S3にアップロード
+    mp3_filename = text_to_speech(description, text_id)
+    audio_file_key = f'uploads/{text_id}/audio/{os.path.basename(mp3_filename)}'
+    s3.upload_file(mp3_filename, S3_BUCKET_NAME, audio_file_key)
+    audio_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{audio_file_key}"
+
+    # DynamoDBにレコードを保存
+    upload_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    dynamodb_record = {
+        'text_id': text_id,
+        'upload_timestamp': upload_timestamp,
+        'additional_text': additional_text,
+        'description': description,
+        'thumbnail_url': responses[0]['file_url'],
+        'media_url': responses[1]['file_url'],
+        'audio_url': audio_url
+    }
+    table.put_item(Item=dynamodb_record)
+
+    return jsonify({"message": "Upload successful", "data": responses, "audio_url": audio_url})
+
+if __name__ == "__main__":
     app.run(debug=True)
